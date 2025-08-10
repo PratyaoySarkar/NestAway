6f36567418774c0ab77a1852a9ce18488e7bcda5
Pratyaoy Sarkar <sarkarpratyaoy@gmail.com>
2025-08-10 19:40:07 +0530

    Added the updated code

--- Client/src/pages/SingleBookingPage.jsx
+++ Client/src/pages/SingleBookingPage.jsx
@@ -23,7 +23,7 @@ export default function SingleBookingPage(){
     return (
         <div className="mt-8 mx-16 px-8 py-8">
             <h1 className="text-3xl font-semibold">{booking.place.title}</h1>
-            <a className="block flex gap-1 font-semibold underline pt-2" target="_blank" href={'https://maps.google.com/?q='+booking.place.address}>
+            <a className="flex gap-1 font-semibold underline pt-2" target="_blank" href={'https://maps.google.com/?q='+booking.place.address}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                     <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
