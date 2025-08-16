327390dc9da764f31292960a56c54ad1787ee5f3
Pratyaoy Sarkar <sarkarpratyaoy@gmail.com>
2025-08-16 18:01:15 +0530

    Added the updated code

--- Client/src/Layout.jsx
+++ Client/src/Layout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
+import Footer from "./Footer";
 
 export default function Layout(){
     return (
         <div className="py-4 px-8 min-h-full-screen">
             <Header />
             <Outlet />
+            <Footer />
         </div>
     )
 }
