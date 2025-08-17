4edfd68c1154b52b98513ff60f822da496b93480
Pratyaoy Sarkar <sarkarpratyaoy@gmail.com>
2025-08-17 23:59:32 +0530

    Added the updated code

--- Client/src/Header.jsx
+++ Client/src/Header.jsx
@@ -42,8 +42,8 @@ export default function Header() {
                 )}
                 {/* Dropdown */}
                 {menuOpen && (
-                    <div className="fixed inset-0 right-0 bg-primary bg-opacity-40 z-50 flex justify-end">
-                        <div className="w-64 bg-primary text-white h-auto shadow-lg relative p-6 flex flex-col mt-16 rounded-l-xl">
+                    <div>
+                        <div className="fixed top-0 right-0 w-64 h-full bg-primary text-white shadow-lg z-50 flex flex-col p-6">
                             <button
                                 onClick={() => setMenuOpen(false)}
                                 className="absolute top-4 right-4 text-white hover:text-gray-300"
