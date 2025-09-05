e582838748b27ee6caa1a6d9adc5f133a609be15
Pratyaoy Sarkar <sarkarpratyaoy@gmail.com>
2025-09-05 23:57:02 +0530

    Added the updated code

--- Client/src/Layout.jsx
+++ Client/src/Layout.jsx
@@ -6,7 +6,7 @@ export default function Layout(){
     return (
         <div className="min-h-screen flex flex-col">
             <Header />
-            <div className="flex-grow px-12 py-4">
+            <div className="flex-grow px-4 py-3 sm:px-6 sm:py-4 md:px-12 md:py-6">
                 <Outlet />
             </div>
             <Footer />
