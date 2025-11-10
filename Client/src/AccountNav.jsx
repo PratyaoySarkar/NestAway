dad50863e370c93ffb2aa5bb7b6ca551ddf5fd6c
Pratyaoy Sarkar <sarkarpratyaoy@gmail.com>
2025-11-10 23:42:35 +0530

    Added the updated code

--- Client/src/AccountNav.jsx
+++ Client/src/AccountNav.jsx
@@ -47,7 +47,7 @@ export default function AccountNav({ userEmail }){
                     flex flex-col gap-4 p-6 shadow-lg transform transition-transform duration-300 ease-in-out
                     ${menuOpen ? "translate-x-0" : "-translate-x-full"}
                     lg:static lg:translate-x-0 lg:w-72 lg:h-auto lg:shadow-none
-                `}>
+                `} style={{ top: "4rem" }} >
                 <Link className={linkClasses('profile')} to={'/account'}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
