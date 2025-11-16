1c75b065381ca45f71a321ad4da50450d0803398
Pratyaoy Sarkar <sarkarpratyaoy@gmail.com>
2025-11-16 13:22:34 +0530

    Added the height updated code

--- Client/src/AccountNav.jsx
+++ Client/src/AccountNav.jsx
@@ -33,21 +33,24 @@ export default function AccountNav({ userEmail }){
 
     const adminEmails = ["sarkarbabu@gmail.com", "sarkar05pra@gmail.com"];
     const isAdmin = adminEmails.includes(userEmail);
+    const headerOffset = "5.5rem";
 
     return (
         <div className="relative">
-            <button className="lg: hidden p-3 focus:outline-none fixed left-4 z-50 top-6 bg-white rounded-full shadow-md"
-            style={{ top: "6rem" }} onClick={() => setMenuOpen(!menuOpen)}>
+            <button className="lg: hidden p-3 focus:outline-none fixed left-4 z-50 bg-white rounded-full shadow-md border border-gray-200"
+            style={{ top: headerOffset }} onClick={() => setMenuOpen(!menuOpen)}>
                 {menuOpen ? <X size={26} /> : <Menu size={26} />}
             </button>
             <nav 
                 ref={sidebarRef}
                 className={`
-                    fixed top-0 left-0 h-full w-64 bg-white border-r border-gray-300 
+                    fixed left-0 h-full w-64 bg-white border-r border-gray-300 
                     flex flex-col gap-4 p-6 shadow-lg transform transition-transform duration-300 ease-in-out
                     ${menuOpen ? "translate-x-0" : "-translate-x-full"}
-                    lg:static lg:translate-x-0 lg:w-72 lg:h-auto lg:shadow-none
-                `} style={{ top: "5.5rem" }} >
+                    lg:static lg:translate-x-0 lg:w-72 lg:h-auto lg:shadow-none 
+                    lg: bg-transparent lg:border-none lg:p-0 z-40
+                `} style={{ top: window.innerWidth < 1024 ? headerOffset : '0',
+                    height: `calc(100vh - ${headerOffset})` }} >
                 <Link className={linkClasses('profile')} to={'/account'}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
